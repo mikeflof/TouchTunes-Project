f61c3fa5ec3b85d7c4e79ce9185ed5be0dba4131
Michael Roubeiz <michaelroubeiz@gmail.com>
2022-01-29 15:49:14 -0500

    Navbar and React files

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       TouchConnect
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
