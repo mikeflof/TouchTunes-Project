ab20206b94811964b3d7727c232f07d3641a1369
melisa1026 <68923209+melisa1026@users.noreply.github.com>
2022-01-29 21:35:25 -0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,11 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import EnterUsernamesPage from "./components/Views/EnterUsernamesPage";
 
+const CLIENT_ID = "03aa3b35defe464e9356b46c90136528"
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
 function App() {
   return (
     // <div className="App">
@@ -25,6 +30,7 @@ function App() {
     <div className="App">
       <Navbar />
       <EnterUsernamesPage />
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
     </div>
   );
 }
